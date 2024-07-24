21fa2f6bb7a59b6e16e6e20ce3f765e9e9febd17
SaidIGUI <saidiguiinfo@gmail.com>
2024-07-24 19:26:22 +0200

    docker fastapi

--- app/oauth2.py
+++ app/oauth2.py
@@ -31,7 +31,8 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     token = verify_access_token(token, credentials_exception)
     user = db.query(models.User).filter(models.User.id == token.id).first()
     return user
